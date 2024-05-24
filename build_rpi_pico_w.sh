125498a4252d5402a413a73ebc340e721c71d5f0
Aron Pavel <ext.aron.pavel@sats.no>
2024-05-24 23:27:42 +0200

    Fixed sed command for darwin systems

--- build_rpi_pico_w.sh
+++ build_rpi_pico_w.sh
@@ -1,16 +1,15 @@
 #!/bin/sh
-# ./build_rpi_pico_w.sh v1.22.2 PicoWExample
+# Command example: ./build_rpi_pico_w.sh v1.22.2 PicoWExample
 
 MPCONFIG_PATH=./micropython/ports/rp2/boards/RPI_PICO_W/mpconfigboard.h
 
 git clone --depth 1 --branch $1 https://github.com/micropython/micropython.git
 
-SEDOPTION="-ri"
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  SEDOPTION="-i ''"
+  sed -i '' 's/\(#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT.*\)".*"/\1"'$2'"/g' $MPCONFIG_PATH
+else
+  sed -ri 's/(#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT.*)".*"/\1"'$2'"/g' $MPCONFIG_PATH
 fi
-
-sed $SEDOPTION 's/(#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT.*)".*"/\1"'$2'"/g' $MPCONFIG_PATH
 
 make -C ./micropython/mpy-cross/ -j 16
 make -C ./micropython/ports/rp2 BOARD=RPI_PICO_W clean
