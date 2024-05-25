1cb3019f6a9a8c5439bfb2d7fb73b92de1409c69
Aron Pavel <ext.aron.pavel@sats.no>
2024-05-25 13:20:18 +0200

    Removed unneeded hostname var

--- build_rpi_pico_w.sh
+++ build_rpi_pico_w.sh
@@ -1,14 +1,15 @@
 #!/bin/sh
 # Command example: ./build_rpi_pico_w.sh v1.22.2 PicoWExample
 
+HOSTNAME=Sesame
 MPCONFIG_PATH=./micropython/ports/rp2/boards/RPI_PICO_W/mpconfigboard.h
 
 git clone --depth 1 --branch $1 https://github.com/micropython/micropython.git
 
 if [[ "$OSTYPE" == "darwin"* ]]; then
-  sed -i '' 's/\(#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT.*\)".*"/\1"'$2'"/g' $MPCONFIG_PATH
+  sed -i '' 's/\(#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT.*\)".*"/\1"'$HOSTNAME'"/g' $MPCONFIG_PATH
 else
-  sed -ri 's/(#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT.*)".*"/\1"'$2'"/g' $MPCONFIG_PATH
+  sed -ri 's/(#define MICROPY_PY_NETWORK_HOSTNAME_DEFAULT.*)".*"/\1"'$HOSTNAME'"/g' $MPCONFIG_PATH
 fi
 
 make -C ./micropython/mpy-cross/ -j 16
